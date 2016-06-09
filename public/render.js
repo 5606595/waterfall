31ed8e08d6839ad9243b983bf1536809ac176818
xiawei <475365254@qq.com>
2016-06-09 16:51:36 +0800

    third

--- public/render.js
+++ public/render.js
@@ -13,6 +13,7 @@ $.get('/request?page=' + page, function(res) {
 $("body").on('mousewheel', function(event) {
     //  jQuery窗口宽度用$(window).height()
     //  jQuery中文档高度为$(document).height()
+    //  JS获取窗口宽度 高度为 document.documentElement.clientWidth, document.documentElement.clientHeight
     if(document.body.scrollTop + $(window).height() + 1000 >= $(document).height()) {
         if(!lock) {
             lock = true;
@@ -41,7 +42,7 @@ function calc() {
     init = display.offsetLeft;
     left = [init];
     temp = $("a");
-    //     获取前n张图片的Left值 并确定arr数组的初始值
+    //     获取前n张图片的Left值 并确定arr数组的初始值 因为绝对定位后offsetLeft不准了 所以用$("#display").offsetLeft + 图片宽度
     for(var i = 0; i < cols; i++) {
         arr.push(temp[i].offsetHeight);
         if(i > 0) {
@@ -49,6 +50,7 @@ function calc() {
             left.push(init);
         }
     }
+    //  将第一行的元素排列好
     for(var i = 0; i < cols; i++) {
         $(temp[i]).css({
             left: left[i] + "px",
@@ -56,6 +58,7 @@ function calc() {
             top: "10px"
         })
     }
+    // 后面开始计算插入
     for(var i = cols, j = temp.length; i < j; i++) {
         height = Math.min.apply(null, arr);
         index = findIndex(height);
